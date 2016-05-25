efe37cfcf53d91a5bda38ca3344e736eface4d00
Benjamin Comeau <benjamin@bcomeau.ca>
2016-05-25 15:40:17 -0400

    Email templates * Email templates for email verification and password reset request * Use the same variable as Parse.com * Update README to explain how to use the new feature

--- index.js
+++ index.js
@@ -5,7 +5,66 @@ var SimpleMailgunAdapter = mailgunOptions => {
   if (!mailgunOptions || !mailgunOptions.apiKey || !mailgunOptions.domain || !mailgunOptions.fromAddress) {
     throw 'SimpleMailgunAdapter requires an API Key, domain, and fromAddress.';
   }
+
+  mailgunOptions.verificationSubject =
+    mailgunOptions.verificationSubject ||
+    'Please verify your e-mail for %appname%';
+  mailgunOptions.verificationBody =
+    mailgunOptions.verificationBody ||
+    'Hi,\n\nYou are being asked to confirm the e-mail address %email% ' +
+    'with %appname%\n\nClick here to confirm it:\n%link%';
+  mailgunOptions.passwordResetSubject =
+    mailgunOptions.passwordResetSubject ||
+    'Password Reset Request for %appname%';
+  mailgunOptions.passwordResetBody =
+    mailgunOptions.passwordResetBody ||
+    'Hi,\n\nYou requested a password reset for %appname%.\n\nClick here ' +
+    'to reset it:\n%link%';
+
+
   var mailgun = Mailgun(mailgunOptions);
+
+  function fillVariables(text) {
+    text = text.replace("%username%", options.user.get("username"));
+    text = text.replace("%email%", options.user.get("email"));
+    text = text.replace("%appname%", options.appName);
+    text = text.replace("%link%", options.link);
+    return text;
+  }
+
+  var sendVerificationEmail = options => {
+    var message = {
+      from: mailgunOptions.fromAddress,
+      to: options.user.get("email"),
+      subject: mailgunOptions.verificationSubject,
+      text: fillVariables(mailgunOptions.verificationBody)
+    }
+    return new Promise((resolve, reject) => {
+      mailgun.messages().send(data, (err, body) => {
+        if (typeof err !== 'undefined') {
+          reject(err);
+        }
+        resolve(body);
+      });
+    });
+  }
+
+  var sendPasswordResetEmail = options => {
+    var message = {
+      from: mailgunOptions.fromAddress,
+      to: options.user.get("email"),
+      subject: mailgunOptions.passwordResetSubject,
+      text: fillVariables(mailgunOptions.passwordResetBody)
+    }
+    return new Promise((resolve, reject) => {
+      mailgun.messages().send(data, (err, body) => {
+        if (typeof err !== 'undefined') {
+          reject(err);
+        }
+        resolve(body);
+      });
+    });
+  }
 
   var sendMail = mail => {
     var data = {
@@ -26,6 +85,8 @@ var SimpleMailgunAdapter = mailgunOptions => {
   }
 
   return Object.freeze({
+    sendVerificationEmail: sendVerificationEmail,
+    sendPasswordResetEmail: sendPasswordResetEmail,
     sendMail: sendMail
   });
 }
